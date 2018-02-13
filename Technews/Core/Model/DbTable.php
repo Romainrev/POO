d11b31ba57d995404e641ff55706ccd9d57cb476
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-02-13 16:53:36 +0100

    Ajout d'une classe abstraite

--- Technews/Core/Model/DbTable.php
+++ Technews/Core/Model/DbTable.php
@@ -20,4 +20,22 @@ abstract class DbTable
 
     # Instance PDO, Object PDO, BDD
     private $_db;
+
+    public function __construct()
+    {
+        # Je récupère l'instance de PDO
+        $this->_db = DbFactory::PdoFactory();
+
+    }
+
+    public function fetchAll(){
+        $sql = "SELECT * FROM" . $this->_table;
+        $sth = $this->_db->prepare($sql);
+        $sth-> execute();
+        return $sth->fetchAll(
+            \PDO::FETCH_CLASS,
+            $this->_classToMap0
+        );
+
+    }
 }
