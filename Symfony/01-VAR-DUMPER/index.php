231bfae6f8d1f20a8b91f4d580857e02c2f265b5
Romainrev <romain.revert92@gmail.com>
2018-02-19 16:55:27 +0100

    Ajout de plusieurs fonctionnalités Symfony

--- Symfony/01-VAR-DUMPER/index.php
+++ Symfony/01-VAR-DUMPER/index.php
@@ -1,3 +1,36 @@
 <?php
 
+
 # Importation de l'Autoload de Composer
+use Symfony\Component\VarDumper\VarDumper;
+
+require_once 'vendor/autoload.php';
+
+# Contenu de Démonstration
+class Contact {
+    private $_firstname,
+            $_lastname;
+
+    /**
+     * Contact constructor.
+     * @param $_firstname
+     * @param $_lastname
+     */
+    public function __construct($_firstname, $_lastname)
+    {
+        $this->_firstname = $_firstname;
+        $this->_lastname = $_lastname;
+    }
+
+
+}
+
+$unString = 'Une chaîne de caractère';
+$unArray  = ['Clement','Hugo','Joffrey'];
+$unObjet  = new Contact('Clement','Allalah');
+
+# Test de VarDumper
+
+VarDumper::dump($unString);
+VarDumper::dump($unArray);
+VarDumper::dump($unObjet);
